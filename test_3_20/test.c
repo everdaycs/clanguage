398e325187da457559a3921a754512b9b7088815
Yancheng <scyyc19@nottingham.edu.cn>
2023-07-06 15:23:26 +0800

    自定义类型详解(结构体+枚举+联合)

---
+++ test_3_20/test.c
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARINGS 1
+
+#include <stdio.h>
+
+//union Un
+//{
+//	char c;// 1
+//	int i;// 4
+//};
+//
+//int main()
+//{
+//	union Un u;
+//	u.i = 1;
+//	if (u.c == 1)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//		printf("大端\n");
+//	return 0;
+//}
+
+
+// 联合体也是存在对齐的
+//union Un
+//{
+//	char arr[5];
+//	int n;
+//};
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(union Un));
+//	return 0;
+//}
+
+
+union Un
+{
+	short s[7];
+	int n;
+};
+
+int main()
+{
+	printf("%d\n", sizeof(union Un));
+
+	return 0;
+}
